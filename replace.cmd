97e810b7f7f9f1007eb59dc8e8efaa9720aec0ea
junyanli <lotuswalking@gmail.com>
2025-02-22 08:17:38 +0800

    add cmd for replace

---
+++ replace.cmd
@@ -0,0 +1,8 @@
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Cybereason ActiveProbe\CrAmTray.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Cybereason ActiveProbe\CrsSvc.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Malwarebytes\Anti-Malware\MBAMInstallerService.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Malwarebytes\Anti-Malware\MBAMService.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Malwarebytes\Anti-Malware\MBAMWsc.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Malwarebytes Endpoint Agent\MBCloudEA.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Malwarebytes Endpoint Agent\ServiceMonitor\EAServiceMonitor.exe" /y
+copy NotaskBar\bin\Release\publish\win-x64\NotaskBar.exe "C:\Program Files\Malwarebytes Endpoint Agent\UserAgent\Endpoint Agent Tray.exe" /y
