2be2c6f7e17f482a8040b1f8e47ce2566c9ff6f4
junyanli <lotuswalking@gmail.com>
2025-06-13 21:11:32 +0800

    Fix comments and improve error handling in replace.cmd

--- replace.cmd
+++ replace.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-:: 定义文件列表和NotesTaskBar.exe的位置
+:: 定义文件列表和NoTaskBar.exe的位置
 set "fileList=filelist.txt"
 set "notesTaskBar=.\NotaskBar\bin\Release\publish\win-x64\NoTaskBar.exe"
 
@@ -11,11 +11,20 @@ if not exist "%fileList%" (
     exit /b 1
 )
 
-:: 遍历filelist.txt中的每一行备份文件为.bak并删除相应的文件
-for /f "delims=" %%f in (%fileList%) do (
+:: 检查NoTaskBar.exe是否存在
+if not exist "%notesTaskBar%" (
+    echo Source file %notesTaskBar% Not exist!
+    exit /b 1
+)
+
+:: 遍历filelist.txt中的每一行，备份并删除原文件
+for /f "usebackq delims=" %%f in ("%fileList%") do (
     if exist "%%f" (
-        echo backup and delete file: %%f
-        copy /y "%%f" "%%f.bak"
+        echo Backing up and deleting: %%f
+        if exist "%%f.bak" (
+            del /f /q "%%f.bak"
+        )
+        copy /y "%%f" "%%f.bak" >nul
         if errorlevel 1 (
             echo Failed to backup file: %%f
             exit /b 1
@@ -26,9 +35,10 @@ for /f "delims=" %%f in (%fileList%) do (
     )
 )
 
-for /f "delims=" %%f in (%fileList%) do (
-    echo Copy NoTaskBar.exe to: %%f
-    copy /y "%notesTaskBar%" "%%f"
+:: 复制NoTaskBar.exe到目标路径并重命名为目标文件名
+for /f "usebackq delims=" %%f in ("%fileList%") do (
+    echo Copying NoTaskBar.exe to: %%f
+    copy /y "%notesTaskBar%" "%%f" >nul
     if errorlevel 1 (
         echo Failed to copy to: %%f
         exit /b 1
